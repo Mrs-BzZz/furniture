f6e18158d472a19bb8fcf44a450d1e82c31041fb
BzZz <adelheid.kaszubi@gmail.com>
2026-07-09 00:17:19 +0200

    Create bookcase.sql

---
+++ rcore_housing/bookcase.sql
@@ -0,0 +1,106 @@
+-- Quick addition to the database. We recommend using the in-game `/housing` command to edit items.
+
+INSERT INTO `rcore_housing_furniture`
+(`object`,`label`,`price`,`img`,`room`,`category`,`colors`,`interactable`,`interactable_type`,`interior`,`exterior`,`is_addon`,`metadata`,`dim_x`,`dim_y`,`dim_z`,`is_blacklisted`,`gamebuild`)
+VALUES
+('bzzz_living_bookcase_a','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015045-uvcbq6-bzzz_living_bookcase_a.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_a2','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015045-56nrkn-bzzz_living_bookcase_a2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_a3','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015045-h34tv4-bzzz_living_bookcase_a3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_b','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015045-1739nj-bzzz_living_bookcase_b.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_b2','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-qat1xk-bzzz_living_bookcase_b2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_b3','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-euxz42-bzzz_living_bookcase_b3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_c','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-q90t1p-bzzz_living_bookcase_c.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_c2','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-czhy2f-bzzz_living_bookcase_c2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_c3','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-8cpex8-bzzz_living_bookcase_c3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_d','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-1yedzl-bzzz_living_bookcase_d.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_d2','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-kssl9g-bzzz_living_bookcase_d2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_d3','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-6t7xzd-bzzz_living_bookcase_d3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_e','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-gnkrzh-bzzz_living_bookcase_e.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_e2','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-dwzi4u-bzzz_living_bookcase_e2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_e3','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-zdlln6-bzzz_living_bookcase_e3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_f','Empty Bookcase',1000.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-res7hb-bzzz_living_bookcase_f.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_f2','Empty Bookcase',1000.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-pwtwug-bzzz_living_bookcase_f2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_f3','Empty Bookcase',1000.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-jefw4d-bzzz_living_bookcase_f3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_g','Bookcase with Books',1500.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-6hb2uv-bzzz_living_bookcase_g.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_g2','Bookcase with Books',1500.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-fqk9pt-bzzz_living_bookcase_g2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_g3','Bookcase with Books',1500.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-0cf77l-bzzz_living_bookcase_g3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_h','Empty Bookcase',900.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-89wdps-bzzz_living_bookcase_h.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_h2','Empty Bookcase',900.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-2pjy15-bzzz_living_bookcase_h2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_h3','Empty Bookcase',900.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-daccwg-bzzz_living_bookcase_h3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_i','Bookcase with Books',1400.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-a6t4z9-bzzz_living_bookcase_i.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_i2','Bookcase with Books',1400.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-gccmoo-bzzz_living_bookcase_i2.png','livingRoom','living_shelving','["brown"]',0,'0',1,1,1,NULL,0,0,0,0,1604),
+('bzzz_living_bookcase_i3','Bookcase with Books',1400.00,'https://bzzz.wiki/wp-content/uploads/1783535015049-555enp-bzzz_living_bookcase_i3.png','livingRoom','living_shelving','["white","grey"]',0,'0',1,1,1,NULL,0,0,0,0,1604);
+
+
+-- Variants
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_a2','a2','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015045-56nrkn-bzzz_living_bookcase_a2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_a';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_a3','a3','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015045-h34tv4-bzzz_living_bookcase_a3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_a';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_b2','b2','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-qat1xk-bzzz_living_bookcase_b2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_b';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_b3','b3','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-euxz42-bzzz_living_bookcase_b3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_b';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_c2','c2','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-czhy2f-bzzz_living_bookcase_c2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_c';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_c3','c3','Empty Bookcase',1200.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-8cpex8-bzzz_living_bookcase_c3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_c';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_d2','d2','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015046-kssl9g-bzzz_living_bookcase_d2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_d';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_d3','d3','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-6t7xzd-bzzz_living_bookcase_d3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_d';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_e2','e2','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-dwzi4u-bzzz_living_bookcase_e2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_e';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_e3','e3','Bookcase with Books',1700.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-zdlln6-bzzz_living_bookcase_e3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_e';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_f2','f2','Empty Bookcase',1000.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-pwtwug-bzzz_living_bookcase_f2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_f';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_f3','f3','Empty Bookcase',1000.00,'https://bzzz.wiki/wp-content/uploads/1783535015047-jefw4d-bzzz_living_bookcase_f3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_f';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_g2','g2','Bookcase with Books',1500.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-fqk9pt-bzzz_living_bookcase_g2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_g';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_g3','g3','Bookcase with Books',1500.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-0cf77l-bzzz_living_bookcase_g3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_g';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_h2','h2','Empty Bookcase',900.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-2pjy15-bzzz_living_bookcase_h2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_h';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_h3','h3','Empty Bookcase',900.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-daccwg-bzzz_living_bookcase_h3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_h';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_i2','i2','Bookcase with Books',1400.00,'https://bzzz.wiki/wp-content/uploads/1783535015048-gccmoo-bzzz_living_bookcase_i2.png',NULL,2,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_i';
+
+INSERT INTO `rcore_housing_furniture_variants`
+(`base_furniture_id`,`variant_object`,`variant_suffix`,`label_override`,`price_override`,`img_override`,`color_variant`,`sort_order`,`active`)
+SELECT `id`,'bzzz_living_bookcase_i3','i3','Bookcase with Books',1400.00,'https://bzzz.wiki/wp-content/uploads/1783535015049-555enp-bzzz_living_bookcase_i3.png',NULL,3,1 FROM `rcore_housing_furniture` WHERE `object`='bzzz_living_bookcase_i';
